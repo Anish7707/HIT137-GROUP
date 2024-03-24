b4788a92bb311c1e3c3b27e58893a68d3c61fec6
Prasanna7707 <163214996+Prasanna7707@users.noreply.github.com>
2024-03-24 21:41:05 +0930

    Added github link.

---
+++ assignment_1.py
@@ -0,0 +1,68 @@
+'''
+Github link : https://github.com/Prasanna7707/HIT137-GROUP.git
+
+                        HIT 137 SOFTWARE NOW
+                            Assignment 1 
+
+
+
+
+
+
+                                                    Group Members Name
+                                       Prasanna Bhakta Shrestha- S345051
+                                              Anish KC Khatri- S355868
+                                               Suraj Adhikari- S357395
+
+
+
+
+
+Python for on Image Processing
+Image processing is a critical subject in artificial intelligence (AI), having a broad impact in industries such as healthcare, autonomous driving, security, and entertainment. In this field, Python has become the go-to language for building image processing models. This is not by chance - Python's user-friendly experience, flexibility, and extensive library support make it a powerful tool for developers and researchers delving into the intricacies of image processing. Let's get into the fundamentals of Python in this domain, exploring its real-world applications, thinking on its impact and ethical considerations, as well as the key tools and modules that enable its capabilities.
+
+The Significance of Image Processing
+Image processing is fundamentally about modifying or analyzing images to obtain insights. Computer vision he¬lps automate things humans cannot easily do alone. For e¬xample, it helps identify pe¬ople in security cameras. It also he¬lps diagnose diseases from me¬dical scans. In a world where digital photos are everywhere, there is an increasing demand for image altering techniques. This points out the importance of flexible tools for professionals working in development and research.
+
+Applications and Use Cases:
+
+Medical Imaging
+In healthcare, image processing is vital for diagnosing diseases, planning treatments, and tracking patient recovery. Python is used in medical care to process images for finding illnesses and planning treatments. It can find tumors in MRI scans, separate cells in microscopes, and discover fractures in X-rays. Deep learning models in Python help radiologists find medical issues accurately.
+
+Autonomous Vehicles
+We all know how TESLA cars are taking the market in the vehicle industry. The idea of self-driving cars is becoming more real, thanks in part to image processing technology. Python is a useful programming language for building self-driving cars. It uses data from car cameras to see lane markers, people, and other vehicles. The OpenCV library in Python is important for real-time image processing in self-driving cars.
+
+Satellite Imagery Analysis
+Python is also used in analyzing images from space. With TensorFlow and Keras, people can classify land, check vegetation, and find areas in danger from natural disasters. Python can help stop deforestation and protect the environment.
+      	
+Societal Impacts and Ethical Considerations
+Python's image processing capabilities can produce beneficial and adverse effects. Early dete-ction of diseases is very important. Me¬dical imaging technology helps find illnesse¬s early. Catching diseases e¬arly can make treatment e¬asier. It can also help kee¬p healthcare costs down. New me¬dical imaging breakthroughs are good for patients. Autonomous vehicles promise a future with safer roads and enhanced accessibility. Yet, ne¬w technologies raise issue¬s: privacy worries, algorithms making biased choices, and risks of misuse¬. For instance, facial recognition stirs debate¬ over surveillance infringing on fre¬edoms. Rules and accountability are needed.
+
+Some of the Python Libraries for Image Processing
+
+Python’s power is in the abundant array of tools and libraries making it a strong friend to developers.
+
+OpenCV: This foundation library for computer vision and image processing has many functions that help in tasks such as object detection, image manipulation etc.
+
+TensorFlow & Keras: The two combine TensorFlow’s extensive tools with Keras’s ease to make them good libraries for building and deploying deep learning models used in image classification or object detection.
+
+Scikit-image: It is built on NumPy and SciPy foundations; this library makes image processing easy from filtering, edge detection, morphology etc., thus serving both beginners and experts well suited for image processing purposes.
+
+Conclusively, Python’s lead in image processing is not accidental. It has simplicity, flexibility and an extensive set of libraries that have contributed to its use cases across diverse fields like health care, autonomous driving and environmental monitoring. However, these steps forward present ethical questions that need intervention. Also, as technology progresses Python remains with researchers and developers as they break new grounds in the field of image processing.
+
+REFERENCES:
+1.	E. Serrano, J. G. Blas, J. Carretero, M. Abella and M. Desco, "Medical Imaging Processing on a Big Data Platform Using Python: Experiences with Heterogeneous and Homogeneous Architectures," 2017 17th IEEE/ACM International Symposium on Cluster, Cloud and Grid Computing (CCGRID), Madrid, Spain, 2017, pp. 830-837, doi: 10.1109/CCGRID.2017.56.
+
+2.	B. Banerjee, S. V. G. and K. M. Buddhiraju, "Satellite image segmentation: A novel adaptive mean-shift clustering based approach," 2012 IEEE International Geoscience and Remote Sensing Symposium, Munich, Germany, 2012, pp. 4319-4322, doi: 10.1109/IGARSS.2012.6351712.
+
+3.	S. Singh, S. Bharadwaj and G. Raj, "Face Recognition Application using MATLAB & Python," 2023 International Conference on Artificial Intelligence and Smart Communication (AISC), Greater Noida, India, 2023, pp. 705-709, doi: 10.1109/AISC56616.2023.10085064.
+
+4.	Sultonov , S. . (2023). IMPORTANCE OF PYTHON PROGRAMMING LANGUAGE IN MACHINE LEARNING. International Bulletin of Engineering and Technology, 3(9), 28–30. Retrieved from: https://internationalbulletins.com/intjour/index.php/ibet/article/view/1020
+
+5.	Bahman Zohuri, Farhang Mossavar-Rahmani, Farahnaz Behgounia,Chapter 26 - Python programming–driven artificial intelligence, Knowledge is Power in Four Dimensions: Models to Forecast Future Paradigm, Academic Press,2022,Pages827-836,ISBN9780323951128, doi:https://doi.org/10.1016/B978-0-323-95112-8.00026-X.
+
+6.	B. Asmika, G. Mounika and P. S. Rani, "Deep Learning for Vision and Decision Making in Self Driving Cars-Challenges with Ethical Decision Making," 2021 International Conference on Intelligent Technologies (CONIT), Hubli, India, 2021, pp. 1-5, doi: 10.1109/CONIT51480.2021.9498342.
+
+7.	I. Stančin and A. Jović, "An overview and comparison of free Python libraries for data mining and big data analysis," 2019 42nd International Convention on Information and Communication Technology, Electronics and Microelectronics (MIPRO), Opatija, Croatia, 2019, pp. 977-982, doi: 10.23919/MIPRO.2019.8757088
+
+'''
